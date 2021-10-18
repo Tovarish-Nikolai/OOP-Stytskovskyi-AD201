733900fad79e8f952c5a52caf918e9469defc6dd
Tovarish-Nikolai <nikocraftex@gmail.com>
2021-10-18 19:54:09 +0300

    Сделал Лаб 6.

---
+++ Lab-6(3)-Stytskovskyi/src/lab/pkg6/pkg3/stytskovskyi/HashInterface.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg6.pkg3.stytskovskyi;
+
+/**
+ *
+ * @author USER
+ */
+interface HashInterface 
+{ int hash(String str); }
